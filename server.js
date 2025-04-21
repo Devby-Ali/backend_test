7cf995d8b1266ed1ea3908ed853573da892d86b0
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-04-21 13:37:55 +0330

    fix bug mongoose.connect method

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 //* Database connection
 (async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     //?error catch
