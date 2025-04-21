9f7e1f6fade46c8980bde274a5efe797be38d852
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-04-21 13:34:17 +0330

    fix bug session time

--- validators/v1/course.js
+++ validators/v1/course.js
@@ -82,7 +82,7 @@ const getOneValidator = yup.object().shape({
 });
 const createSessionValidator = yup.object().shape({
   title: yup.string().required("عنوان جلسه الزامی است"),
-  time: yup.number().required("زمان جلسه الزامی است"),
+  time: yup.string().required("زمان جلسه الزامی است"),
   free: yup
     .number()
     .min(0, "وضعیت رایگان/غیررایگان باید 0 یا 1 باشد")
